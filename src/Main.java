5239b9235da9d53571d5b67f4587754552d8cd5f
xlimitan <xlimitan@yandex.ru>
2023-02-06 13:18:51 +0300

    Задача № 1

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,18 @@ public class Main {
 
     public static void task1 () {
         System.out.println("Задача 1");
+        byte age = 17;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний.");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подаждать.");
+        }
+        age += 2;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний.");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подаждать.");
+        }
 
     }
 
