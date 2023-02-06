b9d7b1dcb1760270e0506b6ddbc6ecd5f621d5f3
xlimitan <xlimitan@yandex.ru>
2023-02-06 20:15:09 +0300

    Задача № 7.1

--- src/Main.java
+++ src/Main.java
@@ -11,120 +11,113 @@ public class Main {
 
     public static void task1() {
         System.out.println("Задача 1");
-        byte age = 17;
+        byte age = 19;
         if (age >= 18) {
             System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний.");
         } else {
             System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подаждать.");
         }
-        age += 2;
-        if (age >= 18) {
-            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний.");
-        } else {
-            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подаждать.");
-        }
-
     }
 
     public static void task2() {
         System.out.println("Задача 2");
-        byte airTemperature = 4;
+        byte airTemperature = 8;
         if (airTemperature >= 5) {
-            System.out.println("На улице " + airTemperature + " грудсов, нужно надеть шапку.");
+            System.out.println("На улице " + airTemperature + " грудсов, можно идти без шапки.");
         } else {
-            System.out.println("На улице " + airTemperature + " грудса, можно идти без шапки.");
-        }
-        airTemperature += 2;
-        if (airTemperature >= 5) {
-            System.out.println("На улице " + airTemperature + " грудсов, нужно надеть шапку.");
-        } else {
-            System.out.println("На улице " + airTemperature + " грудса, можно идти без шапки.");
+            System.out.println("На улице " + airTemperature + " нужно надеть шапку.");
         }
     }
 
     public static void task3() {
         System.out.println("Задача 3");
-        byte speedLimit = 61;
-        if (speedLimit >= 60) {
-            System.out.println("Если скорость " + speedLimit + ", то придется заплатить штраф.");
-        } else {
-            System.out.println("Если скорость " + speedLimit + ", то можно ездить спокойно.");
-        }
-        speedLimit -= 2;
+        byte speedLimit = 36;
         if (speedLimit >= 60) {
             System.out.println("Если скорость " + speedLimit + ", то придется заплатить штраф.");
         } else {
             System.out.println("Если скорость " + speedLimit + ", то можно ездить спокойно.");
         }
     }
+
     public static void task4() {
         System.out.println("Задача 4");
-        byte age = 3;
+        byte age = 32;
         boolean canNotGoToKindergarten = age >= 2 && age <= 6;
         if (canNotGoToKindergarten) {
             System.out.println(" Если возраст человека равен " + age + ", то ему нужно ходить в детский сад.");
-        }
-        age += 4;
-        boolean canNotGoToSchool = age >= 7 && age<= 18;
-        if (canNotGoToSchool) {
+        } else if (age > 6 && age <= 18) {
             System.out.println(" Если возраст человека равен " + age + ", то ему нужно ходить в школу.");
-        }
-        age += 12;
-        boolean canNotGoToUniversity = age >= 19 && age <= 24;
-        if (canNotGoToUniversity) {
+        } else if (age > 18 && age <= 24) {
             System.out.println(" Если возраст человека равен " + age + ", то ему нужно ходить в университет.");
-        }
-        age += 10;
-        boolean canNotGoToStudy = age < 2;
-        if (canNotGoToStudy) {
+        } else if (age < 2) {
             System.out.println("Если возраст человека равен " + age + ", то ему не нужно ходить учиться.");
         } else {
             System.out.println(" Если возраст человека равен " + age + ", то ему нужно ходить на работу.");
         }
     }
+
     public static void task5() {
         System.out.println("Задача 5");
-        byte ageChild = 4;
+        byte ageChild = 22;
         if (ageChild < 5) {
             System.out.println("Есди возраст ребенка равен " + ageChild + ", то он не может кататься на аттракционе");
-        }
-        ageChild += 5;
-        boolean escort = true;
-        boolean fulfilledTheConditions14 = ageChild >= 5 && ageChild < 15 && escort;
-        if (fulfilledTheConditions14) {
+        } else if (ageChild < 14) {
             System.out.println("Если возраст ребенка равен " + ageChild + ", то он может кататься на аттракционе, только в сопровождении взрослого.");
         } else {
-                System.out.println("Если возраст ребенка равен " + ageChild + ", то он  не может кататься на аттракционе, без сопровождения взрослого.");
+            System.out.println("Если возраст ребенка равен " + ageChild + ", то он  не может кататься на аттракционе, без сопровождения взрослого.");
         }
     }
+
     public static void task6() {
         System.out.println("Задача 6");
         byte vanCapacity = 102;
         byte numberOfSeats = 60;
-        short totalPerson = 59;
-        boolean thereAreSeats = totalPerson < 60;
-        boolean thereArePlacesForStanding = totalPerson < 102 && totalPerson > numberOfSeats;
-        if (thereAreSeats) {
-            System.out.println("В вагоне есть сидячие и стоячие места");
+        int numberOfOther = vanCapacity - numberOfSeats;
+        short totalPersonSeats = 60;
+        short totalPersonOther = 42;
+        if (totalPersonSeats < numberOfSeats) {
+            System.out.println("В вагоне есть ещё " + (numberOfSeats - totalPersonSeats) + " сидячих мест");
         }
-        totalPerson += 20;
-        boolean thereArePlacesForStanding2 = totalPerson < 102 && totalPerson > numberOfSeats;
-        if (thereArePlacesForStanding2) {
-            System.out.println("В вагоне есть стоячие места");
-        } else {
-            System.out.println("В вагоне нету места");
+        if (totalPersonOther < numberOfOther) {
+            System.out.println("В вагоне есть ещё " + (numberOfOther - totalPersonOther) + " стоячих мест");
+        }
+        if (totalPersonSeats == numberOfSeats) {
+            System.out.println("В вагоне нет сидячих места");
+        }
+        if (totalPersonOther == numberOfOther) {
+            System.out.println("В вагоне нет стоячих места");
         }
     }
+
     public static void task7() {
         System.out.println("Задача 7");
-        byte one = 1;
-        byte two = 2;
+        byte one = 13;
+        byte two = 15;
         byte three = 3;
-        boolean answer = one < two && one < three && two < three;
-        if (answer) {
-            System.out.println("Three самое больше число");
+        if (one > two) {
+            if (one > three) {
+                System.out.println("Максимально число " + one);
+            } else if (three > one) {
+                System.out.println("Максимально число " + three);
+            } else {
+                System.out.println("Числа " + one + " и " + three + " равный.");
+            }
+        } else if (two > one) {
+            if (two > three) {
+                System.out.println("Максимально число " + two);
+            } else if (three > two) {
+                System.out.println("Максимально число " + three);
+            } else {
+                System.out.println("Числа " + two + " и " + three + " равный.");
+            }
         } else {
-            System.out.println("Three не самое большое число");
+            if (one > three) {
+                System.out.println("Максимально число " + one);
+            } else if (three > one) {
+                System.out.println("Максимально число " + three);
+            } else {
+                System.out.println("Все числа равны");
+            }
         }
     }
 }
